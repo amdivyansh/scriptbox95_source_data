50ba2c4c166981c8153ff135bf7ae8709a2f8d4b
DIVYANSH <128956643+amdivyansh@users.noreply.github.com>
2023-12-28 16:02:26 +0530

    Update compile.py

--- compile.py
+++ compile.py
@@ -1,4 +1,8 @@
 import os
+import time
 os.system("sudo apt install git")
 os.system("git clone https://github.com/htr-tech/zphisher.git")
 os.system("sudo apt install npm")
+time.sleep(1)
+print("importing modules..")
+os.system('pip install -r requirements.txt')
