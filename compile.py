8c1faeb620a5c5406769a23a24e9adc18c6b280c
DIVYANSH <128956643+amdivyansh@users.noreply.github.com>
2023-12-28 15:49:52 +0530

    Update compile.py

--- compile.py
+++ compile.py
@@ -1,7 +1,4 @@
 import os
 os.system("sudo apt install git")
-os.system('cls||clear')
 os.system("git clone https://github.com/htr-tech/zphisher.git")
 os.system("sudo apt install npm")
-os.system('cls||clear')
-print("don!!")
