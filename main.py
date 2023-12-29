070337ce2641bbe39a08f6c1ddcd16892ac66577
DIVYANSH <128956643+amdivyansh@users.noreply.github.com>
2023-12-29 20:56:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,53 +2,78 @@
 from colorama import Fore
 import os
 import time
+from time import sleep
 import sys
 from dta import *
-os.system('cls||clear')
+os.system('clear')
 
-def load():
-    os.system('cls||clear')
-    print(con_inf+"Loading [1%]")
-    time.sleep(2)
-    os.system('cls||clear')
-    print(con_inf+"Loading [10%]")
-    time.sleep(1)
-    os.system('cls||clear')
-    print(con_inf+"Loading [35%]")
-    time.sleep(0.5)
-    os.system('cls||clear')
-    print(con_inf+"Loading [40%]")
-    time.sleep(0.5)
-    os.system('cls||clear')
-    print(con_inf+"Loading [56%]")
-    time.sleep(0.5)
-    os.system('cls||clear')
-    print(con_inf+"Loading [61%]")
-    time.sleep(0.5)
-    os.system('cls||clear')
-    print(con_inf+"Loading [79%]")
-    time.sleep(0.5)
-    os.system('cls||clear')
-    print(con_inf+"Loading [91%]")
-    time.sleep(0.5)
-    os.system('cls||clear')
-    print(con_inf+"Loading [100%]")
-    time.sleep(2)
-    os.system('cls||clear')
+import sys
+from time import sleep
+def handle_user_input():
+    while True:
+        try:
+            print(con+"Select An Option For Your Victim")
+            print(lts)
+            user_option = int(input(con_pls+"Enter your option:"))
+            return user_option
+        except ValueError:
+            print(con_inf+"Error: Please enter an integer.")
+            time.sleep(2)
+            main()
+            """sys.exit(1)"""
+        except KeyboardInterrupt:
+            print('\n'+con_inf+"Exiting ..!!!")
+            sleep(2)
+            sys.exit()
+
+def main():
+    os.system('clear')
     print(go)
-    i = 0
-    while i < 10:
-        print(con_pls+"Select An Option For Your Victim")
-        print(lts)
-        user_input = int(input(con+"Enter your option:"))
+    user_input = handle_user_input()
+    try:
         if user_input < 1 or user_input > 2:
             print(con_inf+"The number you entered is not valid. Try again")
-        if user_input == 1:
+            time.sleep(2)
+            main()
+        elif user_input == 1:
             zphisher()
-        if user_input == 2:
+        elif user_input == 2:
             truecallerjs()
-        
+    except KeyboardInterrupt:
+            print('\n'+con_inf+"Exiting ..!!!")
+            sleep(2)
+            sys.exit()
 
+def load():
+    os.system('clear')
+    print(con_inf+"Loading [1%]")
+    time.sleep(2)
+    os.system('clear')
+    print(con_inf+"Loading [10%]")
+    time.sleep(1)
+    os.system('clear')
+    print(con_inf+"Loading [35%]")
+    time.sleep(0.5)
+    os.system('clear')
+    print(con_inf+"Loading [40%]")
+    time.sleep(0.5)
+    os.system('clear')
+    print(con_inf+"Loading [56%]")
+    time.sleep(0.5)
+    os.system('clear')
+    print(con_inf+"Loading [61%]")
+    time.sleep(0.5)
+    os.system('clear')
+    print(con_inf+"Loading [79%]")
+    time.sleep(0.5)
+    os.system('clear')
+    print(con_inf+"Loading [91%]")
+    time.sleep(0.5)
+    os.system('clear')
+    print(con_inf+"Loading [100%]")
+    time.sleep(2)
+    os.system('clear')
+    main()
 
 
 def truecallerjs():
@@ -66,12 +91,12 @@ def truecallerjs():
     # Install truecallerjs
         os.system("sudo npm install -g truecallerjs")
         print("truecallerjs has been installed.")
-        os.system('cls||clear')
+        os.system('clear')
         os.system("truecallerjs ")
         npm = input(con+"truecallerjs ")
         os.system("truecallerjs "+npm)
 def zphisher():
-    os.system('cls||clear')
+    os.system('clear')
     os.system("bash ./zphisher/zphisher.sh")
 
 
